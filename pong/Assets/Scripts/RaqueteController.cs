ecfd604ec015fc66e917996903e162803dea02bb
igregorioneto <igregoriodoneto@outlook.com>
2024-04-15 14:41:38 -0300

    feat: criando a IA da bola

--- pong/Assets/Scripts/RaqueteController.cs
+++ pong/Assets/Scripts/RaqueteController.cs
@@ -1,14 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class RaqueteController : MonoBehaviour
 {
     private Vector3 minhaPosicao;
     private float meuY;
+    public Transform transformBola;
     public float velocidade;
     public float meuLimite;
     public bool isPlayer1;
+    public bool automatico = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -32,14 +35,21 @@ public class RaqueteController : MonoBehaviour
         {
             // Debug.Log("DOWN");
             meuY -= deltaVelocidade;
-        }
-        if (Input.GetKey(KeyCode.W) && !isPlayer1)
+        }        
+
+        if (Input.GetKey(KeyCode.W) && !isPlayer1 && !automatico)
         {
             meuY += deltaVelocidade;
         }
-        if (Input.GetKey(KeyCode.S) && !isPlayer1)
+        if (Input.GetKey(KeyCode.S) && !isPlayer1 && !automatico)
         {
             meuY -= deltaVelocidade;
+        }
+
+        // Player 2 pegando a posição da bola
+        if (automatico)
+        {
+            meuY = Mathf.Lerp(meuY, transformBola.position.y, 0.02f);
         }
 
         if (meuY < -meuLimite)
