18fad816bffd36e1fe0bbfe9eafb7867e83de3b4
igregorioneto <igregoriodoneto@outlook.com>
2024-04-15 15:05:54 -0300

    feat: adjusting moviment player 2

--- pong/Assets/Scripts/RaqueteController.cs
+++ pong/Assets/Scripts/RaqueteController.cs
@@ -25,30 +25,44 @@ public class RaqueteController : MonoBehaviour
         transform.position = minhaPosicao;
         float deltaVelocidade = velocidade * Time.deltaTime;
 
-
-        if (Input.GetKey(KeyCode.UpArrow) && isPlayer1)
+        // Modo Computador
+        if (!automatico)
         {
-            // Debug.Log("UP");
-            meuY += deltaVelocidade;
-        }
-        if (Input.GetKey(KeyCode.DownArrow) && isPlayer1)
-        {
-            // Debug.Log("DOWN");
-            meuY -= deltaVelocidade;
-        }        
-
-        if (Input.GetKey(KeyCode.W) && !isPlayer1 && !automatico)
-        {
-            meuY += deltaVelocidade;
-        }
-        if (Input.GetKey(KeyCode.S) && !isPlayer1 && !automatico)
-        {
-            meuY -= deltaVelocidade;
-        }
-
-        // Player 2 pegando a posição da bola
-        if (automatico)
-        {
+            if (isPlayer1)
+            {
+                // Player 1
+                if (Input.GetKey(KeyCode.W) && isPlayer1)
+                {
+                    // Debug.Log("UP");
+                    meuY += deltaVelocidade;
+                }
+                if (Input.GetKey(KeyCode.S) && isPlayer1)
+                {
+                    // Debug.Log("DOWN");
+                    meuY -= deltaVelocidade;
+                } 
+            } 
+            else 
+            {
+                if (Input.GetKey(KeyCode.Return))
+                {
+                    automatico = true;
+                }
+                //  Modo Player 2
+                if (Input.GetKey(KeyCode.UpArrow) && !isPlayer1 && !automatico)
+                {
+                    meuY += deltaVelocidade;
+                }
+                if (Input.GetKey(KeyCode.DownArrow) && !isPlayer1 && !automatico)
+                {
+                    meuY -= deltaVelocidade;
+                }
+            }
+        } else {       
+            if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.DownArrow))
+            {
+                automatico = false;
+            }
             meuY = Mathf.Lerp(meuY, transformBola.position.y, 0.02f);
         }
 
