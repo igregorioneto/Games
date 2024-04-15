9ef4af4a85d970c38d78f6d9e35a909a4e7ef76f
igregorioneto <igregoriodoneto@outlook.com>
2024-04-15 16:31:11 -0300

    fix: bug

--- pong/Assets/Scripts/BolaController.cs
+++ pong/Assets/Scripts/BolaController.cs
@@ -20,7 +20,7 @@ public class BolaController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        IniciarJogo();
     }
 
     // Update is called once per frame
