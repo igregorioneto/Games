c3b79a0705e78ee0f98f7bd5e296b79d7cd70846
igregorioneto <igregoriodoneto@outlook.com>
2024-04-16 16:16:31 -0300

    feat: Rigdbody2D Plane

--- FlappyPlane/Assets/Scripts/PlayerController.cs
+++ FlappyPlane/Assets/Scripts/PlayerController.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    private Rigidbody2D meuRB;
+    [SerializeField] private float velocidade = 5f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        meuRB = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            meuRB.velocity = Vector2.up * velocidade;
+        }
     }
 }
